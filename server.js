a93576075b0324e350b96e38aad9812b8309498d
Mostafa17metwallyy <120329212+Mostafa17metwallyy@users.noreply.github.com>
2025-01-22 03:26:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const adminRoutes = require('./routes/adminRoutes'); // Add this line to include
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
